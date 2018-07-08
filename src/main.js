1bb2b6b8979728a24bcc34395a81647cf588641d
lhb <976526980@qq.com>
2018-07-08 11:47:15 +0800

    登入页面样式搭建

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import './assets/css/style.css';
 // 注册elementUI插件
 Vue.use(ElementUI);
 
