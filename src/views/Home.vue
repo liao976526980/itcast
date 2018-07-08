8c54402ad5cb8df98f56115d2d45588c587a60ad
lhb <976526980@qq.com>
2018-07-08 19:31:26 +0800

    头部基本样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
     <el-container class="container">
-        <el-header class="header">Header</el-header>
+        <!-- 头部样式 -->
+        <el-header class="header">
+            <!-- 栅格系统布局 -->
+            <el-row>
+                <el-col :span="4">
+                    <div class="grid-content bg-purple"><img src="/static/logo.png" alt=""></div>
+                </el-col>
+                <el-col :span="19"><div class="grid-content bg-purple-light"><h2 class="middle">电商后台管理系统</h2></div></el-col>
+                <el-col :span="1"><div class="grid-content bg-purple">
+                    <a href="#" class="logout">退出</a>
+                    </div></el-col>
+            </el-row>
+        </el-header>
         <el-container>
             <el-aside class="aside" width="200px">Aside</el-aside>
             <el-main class="main">Main</el-main>
@@ -19,6 +31,19 @@ export default {};
 
 .header {
   background-color: #b3c0d1;
+  padding: 0;
+}
+
+.header .middle {
+    line-height: 60px;
+    color: #fff;
+    text-align: center;
+}
+
+.header .logout {
+    line-height: 60px;
+    text-decoration: none;
+    color: gold;
 }
 
 .aside {
