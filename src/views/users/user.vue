81108ca4c302a5a26c952d2dbc06f4ac9d35241d
lhb <976526980@qq.com>
2018-07-10 21:17:51 +0800

    添加用户按钮弹出层完成

--- src/views/users/user.vue
+++ src/views/users/user.vue
@@ -1,99 +1,122 @@
 <template>
     <el-card class="box-card">
       <!-- 面包屑 -->
-            <el-breadcrumb separator-class="el-icon-arrow-right">
-              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-              <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-              <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-            </el-breadcrumb>
-            <!-- 搜索区域 -->
-            <el-row class="searchArea">
-              <el-col :span="24">
-                <!-- 搜索功能
-                    1.绑定搜索文本框
-                    2.给按钮搜索绑定事件
-                 -->
-                <el-input v-model="searchValue" placeholder="请输入内容" class="searchInput" clearable>
-                  <el-button @click="handleSearch" slot="append" icon="el-icon-search"></el-button>
-                </el-input>
-                <el-button type="success" plain>添加用户</el-button>
-              </el-col>
-            </el-row>
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <!-- 搜索区域 -->
+      <el-row class="searchArea">
+        <el-col :span="24">
+          <!-- 搜索功能
+              1.绑定搜索文本框
+              2.给按钮搜索绑定事件
+            -->
+          <el-input v-model="searchValue" placeholder="请输入内容" class="searchInput" clearable>
+            <el-button @click="handleSearch" slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button @click="addUserDialogVisible = true" type="success" plain>添加用户</el-button>
+        </el-col>
+      </el-row>
 
-            <!-- 表格 -->
-            <el-table
-              v-loading="loading"
-              :data="list"
-              stripe
-              border
-              style="width: 100%">
-              <el-table-column
-                type="index"
-                width="50">
-              </el-table-column>
-              <el-table-column
-                prop="username"
-                label="姓名"
-                width="180">
-              </el-table-column>
-              <el-table-column
-                prop="email"
-                label="邮箱"
-                width="180">
-              </el-table-column>
-              <el-table-column
-                prop="mobile"
-                label="电话">
-              </el-table-column>
-              <el-table-column label="创建日期">
-                <template slot-scope="scope">
-                  {{ scope.row.create_time | fmtDate('YYYY-MM-DD') }}
-                </template>
-              </el-table-column>
-              <el-table-column label="用户状态" width="100">
-                <template slot-scope="scope">
-                  <el-switch
-                    @change="handleSwitchChange(scope.row)"
-                    v-model="scope.row.mg_state"
-                    active-color="#13ce66"
-                    inactive-color="#ff4949">
-                  </el-switch>
-                </template>
-              </el-table-column>
-              <el-table-column label="操作">
-                <template slot-scope="scope">
-                    <el-button
-                      plain
-                      size="mini"
-                      type="primary"
-                      icon="el-icon-edit">
-                    </el-button>
-                    <el-button
-                      @click="handleDelete(scope.row.id)"
-                      plain
-                      size="mini"
-                      type="danger"
-                      icon="el-icon-delete">
-                    </el-button>
-                    <el-button
-                      plain
-                      size="mini"
-                      type="warning"
-                      icon="el-icon-check">
-                    </el-button>
-                  </template>
-              </el-table-column>
-            </el-table>
-            <!-- 分页 -->
-            <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="currentPage"
-              :page-sizes="[ 4, 6, 8]"
-              :page-size="pageSize"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="total">
-            </el-pagination>
+      <!-- 表格 -->
+      <el-table
+        v-loading="loading"
+        :data="list"
+        stripe
+        border
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="username"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="email"
+          label="邮箱"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="mobile"
+          label="电话">
+        </el-table-column>
+        <el-table-column label="创建日期">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | fmtDate('YYYY-MM-DD') }}
+          </template>
+        </el-table-column>
+        <el-table-column label="用户状态" width="100">
+          <template slot-scope="scope">
+            <el-switch
+              @change="handleSwitchChange(scope.row)"
+              v-model="scope.row.mg_state"
+              active-color="#13ce66"
+              inactive-color="#ff4949">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+              <el-button
+                plain
+                size="mini"
+                type="primary"
+                icon="el-icon-edit">
+              </el-button>
+              <el-button
+                @click="handleDelete(scope.row.id)"
+                plain
+                size="mini"
+                type="danger"
+                icon="el-icon-delete">
+              </el-button>
+              <el-button
+                plain
+                size="mini"
+                type="warning"
+                icon="el-icon-check">
+              </el-button>
+            </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[ 4, 6, 8]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+      <!-- 添加用户的弹出框 -->
+      <el-dialog title="添加用户" :visible.sync="addUserDialogVisible">
+        <el-form
+          label-width="100px"
+          :model="formData">
+          <el-form-item label="用户名">
+            <el-input v-model="formData.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input type="password" v-model="formData.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="formData.email" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="电话">
+            <el-input v-model="formData.mobile" auto-complete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="addUserDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addUserDialogVisible = false">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-card>
 </template>
 
@@ -110,7 +133,15 @@ export default {
       // 总共的数据条数,从服务器获取
       total: 0,
       // 绑定搜索文本框
-      searchValue: ''
+      searchValue: '',
+      // 控制添加用户的对话显示和隐藏
+      addUserDialogVisible: false,
+      formData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      }
     };
   },
   created() {
